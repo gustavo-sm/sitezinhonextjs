321a64d27b6b333a7037810d88d2b2a2d3e080cb
gus <gustavo1sad@gmail.com>
2023-03-10 20:52:07 -0300

    feat: Nova mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
  function Home() {
-    return <h1>Teste</h1>
+    return (
+    <>  
+        <h1 style={{color:"red"}}>Cuidado!</h1>
+        <h3>Ultimamente o sistema vem detectando altos niveis de estresse no seu trabalho! <br/>Favor considerar a saúde em primeiro lugar</h3>
+    </>);
+    
  }
 
  export default Home;
