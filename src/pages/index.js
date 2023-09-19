8e154691493b444f75b3b8e81e6b2358613114c1
gustavo1sad@gmail.com <gustavo1sad@gmail.com>
2023-09-19 13:28:17 -0300

    Alterado estrutura de pastas e alterado conteúdo

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+// import { useEffect, useState } from "react";
+import style from "../assets/styles/globals.module.css";
+
+function Home() {
+    
+    return (
+        <>
+            <h1 className={style.heading} style = {{color:"red"}}>In the beginning created God the heavens and the earth :)</h1>    
+        </>);
+
+}
+
+export default Home;
