403c5db4dc2579c9b596eb41c21f9db2c6602b2e
gustavo-sm <gustavo1sad@gmail.com>
2023-09-20 04:46:11 -0300

    Alterado conteúdo para Em Construçao

--- src/pages/components/header/header.js
+++ src/pages/components/header/header.js
@@ -1,7 +1,7 @@
 import styles from "./component.module.css";
 
 function Header() {
-    return (<h1 className={styles.parag}>In the beginning created God the heavens and the earth :)</h1>);
+    return (<h1 className={styles.parag}>Em construção :)</h1>);
 }
 
 export default Header;
