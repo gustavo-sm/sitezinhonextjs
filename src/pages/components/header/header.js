c034ea963297e7e703795713478322f22577ba69
gustavo-sm <gustavo1sad@gmail.com>
2023-09-20 05:21:33 -0300

    Incluido icone do stackoverflow

--- src/pages/components/header/header.js
+++ src/pages/components/header/header.js
@@ -1,10 +1,11 @@
 import styles from "./component.module.css";
 import {AiFillLinkedin} from "react-icons/ai"
+import {LiaStackOverflow} from "react-icons/lia"
 
 function Header() {
     return (
         <>
-            <h1 className={styles.heading}>Em construção, depois de várias pesquisas no stackoverflow espero logo terminar a V1 😅 </h1>
+            <h1 className={styles.heading}>Em construção, depois de várias pesquisas no <LiaStackOverflow/> espero logo terminar a V1 😅 </h1>
             <a className={styles.url} href="https://linkedin.com/in/gustavo-sm" target="_blank"><AiFillLinkedin/></a>
         </>
 
