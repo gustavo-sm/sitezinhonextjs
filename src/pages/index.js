7dfb378ef0b329580921acfbbb3e8304d4512707
gustavo-sm <gustavo1sad@gmail.com>
2023-09-20 04:46:32 -0300

    Centralizado div de header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 // import { useEffect, useState } from "react";
+import style from './index.module.css'
 import Header from "./components/header/header";
 
 function Home() {
     
     return (
-        <>
+        <div className={style.centered}>
             <Header/>
-        </>);
+        </div>);
 
 }
 
