b561600586f303507505c2d27741d7383b61f0ab
gustavo-sm <gustavo1sad@gmail.com>
2023-09-20 05:18:12 -0300

    Alterado texto

--- src/pages/components/header/header.js
+++ src/pages/components/header/header.js
@@ -4,7 +4,7 @@ import {AiFillLinkedin} from "react-icons/ai"
 function Header() {
     return (
         <>
-            <h1 className={styles.heading}>Em construção, depois de várias pesquisas no stackoverflow espero terminar a V1 😅 </h1>
+            <h1 className={styles.heading}>Em construção, depois de várias pesquisas no stackoverflow espero logo terminar a V1 😅 </h1>
             <a className={styles.url} href="https://linkedin.com/in/gustavo-sm" target="_blank"><AiFillLinkedin/></a>
         </>
 
