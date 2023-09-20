b4d3bb44ab650ad16a731b81e145f5b5cdcd8392
gustavo-sm <gustavo1sad@gmail.com>
2023-09-20 04:30:38 -0300

    Criado componente para headerr

---
+++ src/pages/components/header/header.js
@@ -0,0 +1,7 @@
+import styles from "./component.module.css";
+
+function Header() {
+    return (<h1 className={styles.parag}>In the beginning created God the heavens and the earth :)</h1>);
+}
+
+export default Header;
