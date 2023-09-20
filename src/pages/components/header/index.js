1ebf325f4c1c507d2dc679bb76542f35b531e3ba
gustavo-sm <gustavo1sad@gmail.com>
2023-09-20 07:15:11 -0300

    Alteração de texto principal

--- src/pages/components/header/index.js
+++ src/pages/components/header/index.js
@@ -5,7 +5,9 @@ import {LiaStackOverflow} from "react-icons/lia"
 function Header() {
     return (
         <div>
-            <h1 className={styles.heading}>Em construção, depois de várias pesquisas no <LiaStackOverflow/> espero logo terminar a V1 😅 </h1>
+            <h1 className={styles.heading}>Em construção! <br/>
+                Depois de várias pesquisas no <LiaStackOverflow/>, espero logo terminar a V1 😅 
+            </h1>
         </div>
 
     );
