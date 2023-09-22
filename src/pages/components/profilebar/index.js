44a5008adeb799e8feed8f51239ff53acce81dae
gustavo1sad@gmail.com <gustavo1sad@gmail.com>
2023-09-21 23:47:45 -0300

    Adicionado seção para redes sociais

--- src/pages/components/profilebar/index.js
+++ src/pages/components/profilebar/index.js
@@ -1,12 +1,18 @@
 import styles from "./component.module.css";
-import {BiLogoLinkedin} from 'react-icons/bi'
+import {BiLogoLinkedin, BiLogoGithub} from 'react-icons/bi'
 
 function ProfileBar() {
     return(
         <div className={styles.center_bar}>
-            <a className={styles.linkedin_icon} href="https://linkedin.com/in/gustavo-sm" target="_blank">
-                <BiLogoLinkedin size={30}/>
-            </a>
+            <div id={styles.inner_div}>
+                <p className={styles.parag_smaller}>Redes sociais:</p>
+                <a id={styles.linkedin_icon} className={styles.icons_spacing} href="https://linkedin.com/in/gustavo-sm" target="_blank">
+                    <BiLogoLinkedin size={30}/>
+                </a>
+                <a id={styles.github_icon} className={styles.icons_spacing} href="github.com/gustavo-sm/" target="_blank">
+                    <BiLogoGithub size={30}/>
+                </a>
+            </div>
         </div>
     );
 
