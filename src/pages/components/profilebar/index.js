3f44ad1979a132b61f80a1a87489c8756db4f75c
gustavo1sad@gmail.com <gustavo1sad@gmail.com>
2023-09-21 23:49:50 -0300

    Corrigido URL do github

--- src/pages/components/profilebar/index.js
+++ src/pages/components/profilebar/index.js
@@ -9,7 +9,7 @@ function ProfileBar() {
                 <a id={styles.linkedin_icon} className={styles.icons_spacing} href="https://linkedin.com/in/gustavo-sm" target="_blank">
                     <BiLogoLinkedin size={30}/>
                 </a>
-                <a id={styles.github_icon} className={styles.icons_spacing} href="github.com/gustavo-sm/" target="_blank">
+                <a id={styles.github_icon} className={styles.icons_spacing} href="https://github.com/gustavo-sm/" target="_blank">
                     <BiLogoGithub size={30}/>
                 </a>
             </div>
