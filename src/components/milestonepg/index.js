637a4a94e64804e53863c08c5005ecb3da2b62df
gustavo1sad@gmail.com <gustavo1sad@gmail.com>
2023-09-26 13:07:00 -0300

    Código refatorado, separação de responsabilidades

--- src/components/milestonepg/index.js
+++ src/components/milestonepg/index.js
@@ -1,40 +1,58 @@
 import style from './component.module.css';
 import ProgressBar from "@ramonak/react-progress-bar";
-import {useEffect, useState } from "react";
+import OctoRequest from "../../services/Github/GithubAPI";
+import { useEffect, useState } from "react";
+
+async function getMilestoneData(id = -1) {
+    const octo = OctoRequest();
+    octo.initOctokitUnAuth();
+    return await octo.GETRequest(`/repos/gustavo-sm/sitezinhonextjs/milestones/${id}`, {});
+}
 
 function MilestonePG(props) {
-    const [open_issues, setOpenIssues] = useState(0);
-    const [closed_issues, setClosedIssues] = useState(0);
-    const [milestone_title, setMilestoneTitle] = useState('');
-    const [milestone_url, setMilestoneUrl] = useState('');
+    const [milestone_data, setMilestoneData] = useState({});
+    const [fetch_error, setFetchError] = useState('');
     const [percentage, setPercentage] = useState(0);
-    
-    function calcPercentage(){
-        const value = (closed_issues*100)/(open_issues+closed_issues);
+
+    function calcPercentage(open_issues, closed_issues) {
+        const value = (closed_issues * 100) / (open_issues + closed_issues);
         setPercentage(value.toFixed(2));
     }
 
-    useEffect(()=> {
-        setOpenIssues(props.open);
-        setClosedIssues(props.closed);
-        setMilestoneTitle(props.title);
-        setMilestoneUrl(props.link);
+    useEffect(() => {
+        const fetchData = async () => {
+            const promise_return = await getMilestoneData(props.milestone_id);
+            console.log(promise_return.data);
+            if (promise_return.error)
+                setFetchError(promise_return.message);
 
-        calcPercentage();
-    });
+            else
+                setMilestoneData(promise_return.data);
 
+            calcPercentage(promise_return.data.open_issues, promise_return.data.closed_issues);
+
+        }
+        fetchData();
+    }, [setMilestoneData, setFetchError, setPercentage]);
 
 
     return (
         <div>
-            <p className={style.text}>
-                Progresso da Milestone:&nbsp;
-                <a className={style.link} href={milestone_url} target='_blank'>
-                    {milestone_title}
-                </a>
-            </p>
-            <ProgressBar id={style.wrapper} completed={percentage}/>
-            <p style = {{float:'right'}}className={style.text}>{closed_issues}/{open_issues + closed_issues}</p>
+            {!fetch_error ? 
+                <>
+                <p className={style.text}>
+                    Progresso da Milestone:&nbsp;
+                    <a className={style.link} href='' target='_blank'>
+                        {milestone_data.title}
+                    </a>
+                </p>
+                <ProgressBar id={style.wrapper} completed={percentage}/>
+                <p style = {{float:'right'}} className={style.text}>
+                    {milestone_data.closed_issues}/{milestone_data.open_issues + milestone_data.closed_issues}
+                </p>
+                </>
+                : <p>Erro na request da Milestone: {fetch_error}</p>
+            }
         </div>
 
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,47 +2,15 @@ import style from './index.module.css'
 import Header from "../components/header";
 import ProfileBar from '../components/profilebar';
 import MilestonePG from '../components/milestonepg';
-import OctoRequest from "../services/Github/GithubAPI";
-import { useEffect, useState } from "react";
-
-async function getMilestoneData() {
-    const octo = OctoRequest();
-    octo.initOctokitUnAuth();
-    return await octo.GETRequest("/repos/gustavo-sm/sitezinhonextjs/milestones/1", {});
-}
 
 function Home() {
-    const [fetch_error, setError] = useState('');
-    const [milestone_data, setMilestoneData] = useState({});
-
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const promise_return = await getMilestoneData();
-
-            if(promise_return.error)
-                setError(promise_return.message);
-
-            else
-                setMilestoneData(promise_return.data);
-        }
-        fetchData();
-    }, [setMilestoneData, setError]);
 
     return (
         <div className={style.centered_content}>           
             <ul id={style.content_list}>
                 <Header/>
                 <ProfileBar/> 
-                {!fetch_error ?
-                    <MilestonePG 
-                    open = {milestone_data.open_issues} 
-                    closed = {milestone_data.closed_issues} 
-                    title = {milestone_data.title} 
-                    link = {milestone_data.html_url}/>
-                : <p>Erro na request da milestone: {fetch_error}</p>
-                }
-
+                <MilestonePG milestone_id={1}/>
 
             </ul>
         </div>
