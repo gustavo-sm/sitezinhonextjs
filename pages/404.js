cbc9b32a3c0decf2e016f0ea7ede128a928c3c74
gustavo-sm <gustavo1sad@gmail.com>
2023-09-28 02:26:23 -0300

    Página para 404

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import style from './index.module.css'
+
+export default function NotFound() {
+    return (
+        <>
+
+            <div className={style.centered_content}>
+                <h2>Como nem tudo é determinístico na computação, melhor voltar pra&nbsp;
+                    <a href='/'>home</a>
+                </h2>
+            </div>
+        </>
+    );
+}
