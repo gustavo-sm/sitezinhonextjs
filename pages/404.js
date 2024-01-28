064fe5f04188c211c13c3e99a1a0b652a07e1bbb
gustavo-sm <gustavo1sad@gmail.com>
2024-01-28 13:39:09 -0300

    Alterando texto 404

--- pages/404.js
+++ pages/404.js
@@ -7,8 +7,8 @@ export default function NotFound() {
             <div className={style.centered_content}>
                 <ul id={style.content_list}>
                     <h1>404</h1>
-                    <h2>Como nem tudo é determinístico na computação, melhor voltar pra&nbsp;
-                        <a href='/'>home</a>
+                    <h2>A página não existe, e talvez nunca existirá xD&nbsp; <br/>
+                        Voltar para <a href='/'>home</a>
                     </h2>
                 </ul>
 
